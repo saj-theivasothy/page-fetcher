624efe79e4d65671c979c87f5ed140207c70ccc1
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-12 01:12:50 +0000

    implemented page downloader

---
+++ fetcher.js
@@ -0,0 +1,56 @@
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const req = function(write) {
+  const input = process.argv.slice(2);
+  request(input[0], (error, body) => {
+    if (error) {
+      console.log('error: ', error);
+    } else {
+      if ()
+      if (fs.existsSync(input[1])) {
+        ifOverwrite(() => {
+          write(body.body, input[1]);
+          rl.close();
+        });
+
+      } else {
+        write(body.body, input[1]);
+        rl.close();
+      }
+    }
+  });
+};
+
+const ifOverwrite = (callback) => {
+  
+
+  rl.question('File already exists, do you want to overwrite the file? ', (answer) => {
+    (answer === 'y' || answer === 'Y') ? callback() : process.exit();
+  });
+};
+  
+const writeToFile = function(data, output) {
+  fs.writeFile(output, data, 'utf8', (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const stats = fs.statSync(output);
+      const size = stats.size;
+      console.log(`Downloaded and saved ${size} bytes to ${output}`);
+    }
+  });
+    
+};
+  
+
+  
+
+
+req(writeToFile);
